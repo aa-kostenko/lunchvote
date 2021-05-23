57cff24c539269e54207655cf1cf32826de7f3ce
dev <dev@example.com>
2021-05-23 20:23:10 +0300

    Modify delete method for RestaurantRepository

--- src/main/java/org/example/lunchvote/repository/RestaurantRepository.java
+++ src/main/java/org/example/lunchvote/repository/RestaurantRepository.java
@@ -2,9 +2,11 @@ package org.example.lunchvote.repository;
 
 import org.example.lunchvote.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +14,11 @@ import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
 
     @Query("SELECT r FROM Restaurant r JOIN FETCH r.lunchMenuItems WHERE r.id = :id")
     Optional<Restaurant> getWithLunchMenuItems(@Param("id") int id);
